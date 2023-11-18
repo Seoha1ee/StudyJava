0e05667d8c8fe165251733fd3e4cf8e6217c7095
1eeseo <din1410@naver.com>
2023-11-18 15:08:08 +0900

    [1003] 출력하기03

---
+++ src/main/java/study_100/Code_1003.java
@@ -0,0 +1,11 @@
+package study_100;
+
+public class Code_1003 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+
+        // 개행 메서드 사용
+        System.out.println("Hello" + System.lineSeparator() + "World");
+    }
+}
