85248122b5569c9cf3a952b83cc271bb98fcd3f9
1eeseo <din1410@naver.com>
2023-11-20 21:25:25 +0900

    [1011] [기초-입출력] 문자 1개 입력받아 그대로 출력하기

---
+++ src/main/java/study_100/Code_1011.java
@@ -0,0 +1,11 @@
+package study_100;
+
+import java.util.Scanner;
+
+public class Code_1011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a =sc.nextLine();
+        System.out.println(a);
+    }
+}
