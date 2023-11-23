a6afbbf313d7d8acd198771790131759a0c5615d
1eeseo <din1410@naver.com>
2023-11-23 20:05:53 +0900

    [1014] : [기초-입출력] 문자 2개 입력받아 순서 바꿔 출력하기

---
+++ src/main/java/study_100/Code_1014.java
@@ -0,0 +1,16 @@
+package study_100;
+
+import java.util.Scanner;
+
+public class Code_1014 {
+    public static void main(String[] args) {
+        String x;
+        String y;
+
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextLine();
+        y = sc.nextLine();
+
+        System.out.println(y+x);
+    }
+}
