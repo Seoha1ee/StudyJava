08f04f23e7d9a44e3d679dcc3f6b7d6d47f1f648
1eeseo <din1410@naver.com>
2023-11-25 18:57:04 +0900

    [1015] : [기초-입출력] 실수 입력받아 둘째 자리까지 출력하기

---
+++ src/main/java/study_100/Code_1015.java
@@ -0,0 +1,13 @@
+package study_100;
+
+
+import java.util.Scanner;
+
+public class Code_1015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Float a = sc.nextFloat();
+        System.out.println(String.format("%.2f",a));
+
+    }
+}
