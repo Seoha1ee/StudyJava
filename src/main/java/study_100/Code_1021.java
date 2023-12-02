a609faae63d5aa11af1d7b214418c3b09e353dd6
Seoha1ee <din1410@naver.com>
2023-12-02 10:06:50 +0900

    [1021] : [기초-입출력] 단어 1개 입력받아 그대로 출력하기

---
+++ src/main/java/study_100/Code_1021.java
@@ -0,0 +1,11 @@
+package study_100;
+
+import java.util.Scanner;
+
+public class Code_1021 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char [] c = sc.next().toCharArray();
+        System.out.println(c);
+    }
+}
