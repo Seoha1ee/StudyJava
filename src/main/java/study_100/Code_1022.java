ec5d9c4bb66ea173823885ec87a8deea9dd0e016
Seoha1ee <din1410@naver.com>
2023-12-02 10:09:19 +0900

    [1022 : [기초-입출력] 문장 1개 입력받아 그대로 출력하기(설명)

---
+++ src/main/java/study_100/Code_1022.java
@@ -0,0 +1,12 @@
+package study_100;
+
+import java.util.Scanner;
+
+public class Code_1022 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s);
+
+    }
+}
