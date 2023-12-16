8b0a00c39cc2c8252c7a7b28484468dc0d03e4e2
1eeseo <din1410@naver.com>
2023-12-16 19:36:43 +0900

    [1029] : [기초-데이터형] 실수 1개 입력받아 그대로 출력하기2(설명)

---
+++ src/main/java/study_100/Code_1029.java
@@ -0,0 +1,11 @@
+package study_100;
+
+import java.util.Scanner;
+
+public class Code_1029 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double d = sc.nextDouble();
+        System.out.printf("%.11f",d);
+    }
+}
