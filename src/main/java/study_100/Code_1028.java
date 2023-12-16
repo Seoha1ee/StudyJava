03c70c06faf0398d69e8f2090489d38595cb0491
1eeseo <din1410@naver.com>
2023-12-16 19:38:02 +0900

    [1028] : [기초-데이터형] 정수 1개 입력받아 그대로 출력하기2(설명

---
+++ src/main/java/study_100/Code_1028.java
@@ -0,0 +1,10 @@
+package study_100;
+
+import java.util.Scanner;
+public class Code_1028 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Long a = sc.nextLong();
+        System.out.printf("%d", a);
+    }
+}
