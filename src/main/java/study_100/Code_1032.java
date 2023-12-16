f149d3f36c2a1c3ebaba77488172fd086cf1393c
1eeseo <din1410@naver.com>
2023-12-16 19:37:41 +0900

    [1032] : [기초-출력변환] 10진 정수 입력받아 16진수로 출력하기1

---
+++ src/main/java/study_100/Code_1032.java
@@ -0,0 +1,11 @@
+package study_100;
+
+import java.util.Scanner;
+
+public class Code_1032 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.printf("%x",a);
+    }
+}
