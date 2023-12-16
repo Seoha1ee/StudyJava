cbd613b9692c3d06e537e92029110f129ca3ac18
1eeseo <din1410@naver.com>
2023-12-16 19:37:26 +0900

    [1031] : [기초-출력변환] 10진 정수 1개 입력받아 8진수로 출력하기

---
+++ src/main/java/study_100/Code_1031.java
@@ -0,0 +1,12 @@
+package study_100;
+
+import java.util.Scanner;
+
+public class Code_1031 {public static void main(String[] args) {
+    // 10진 정수를 입력받아 8진수로 출력
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    System.out.printf("%o",a);
+}
+
+}
